d9041ea5d2230690c2e4d3afb1c968aa992d6af2
Knexy04 <nikitos.markin04@gmail.com>
2024-03-02 23:38:17 +0300

    efwe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,12 +1,10 @@
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
+
 
 const Home = () => {
 
   return (
     <>
-      <Header />
-      <Footer />
+      
     </>
   );
 };
