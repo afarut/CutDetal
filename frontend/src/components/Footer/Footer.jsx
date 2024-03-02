734506566dcfa38c917f6b8261a446b0505424ad
Daniil Markov <reedtzhere@gmail.com>
2024-03-03 00:27:27 +0300

    [Frontend] - chipichipi

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="footer-wrapper w-full">
+        <div className="footer-wrapper appContainer w-full">
             footer
         </div>
     )
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,35 @@
-import module from './Header.module.css'
+import module from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <div className={`${module.wrapper} w-full h-[150px] text-3xl font-bold underline`}>
-            header
-        </div>
-    )
-}
+  return (
+    <div
+      className={`${module.wrapper} w-[100%] h-[82px] appContainer flex justify-between`}
+    >
+      <div className="flex justify-center items-center">
+        <span className={`${module.logoTitle}`}>LOGO</span>
+      </div>
 
-export default Header
+      <div className="flex items-center">
+        <div className={`${module.navigationWrapper}`}>
+          <ul className="flex justify-center items-center mr-[10px]">
+            <li>Контакты</li>
+            <li>Помощь</li>
+            <li>Бизнес</li>
+            <li>О нас</li>
+          </ul>
+        </div>
+
+        <div className={`${module.userAuthButtonWrapper} flex items-center`}>
+          <div className={`${module.signUpButton}`}>
+            <button>Регистрация</button>
+          </div>
+          <div>
+            <button>Логин</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
