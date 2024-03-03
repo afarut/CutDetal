4115607062c19e4b98371473d216ed0eadb85c10
afarut <renat030205@ya.ru>
2024-03-03 16:58:02 +0300

    add models

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 
-class Range(models.Model):
-	start = models.FloatField()
-	stop = models.FloatField()
-	price = models.FloatField()
-
-	def __str__(self):
-		return f"{self.start} - {self.stop} by {self.price}"
-
-
 class Material(models.Model):
 	name = models.CharField(max_length=30)
 	weight = models.FloatField()
@@ -18,6 +9,24 @@ class Material(models.Model):
 		return self.name
 
 
-class MaterialRange(Range):
+class Range(models.Model):
+	start = models.FloatField()
+	stop = models.FloatField()
+	price = models.FloatField()
 	material = models.ForeignKey(Material, on_delete=models.CASCADE)
 
+	def __str__(self):
+		return f"{self.start} - {self.stop} by {self.price}"
+
+
+#class Detail(models.Model):
+#	material = models.ForeignKey(Material, on_delete=models.CASCADE)
+#	width = models.FloatField()
+#	height = models.FloatField()
+#	dxf_file = models.FileField(upload_to="dxf")
+#	swg_file = models.FileField(upload_to="swg")
+#	srtab = models.FloatField()
+#	srlength = models.FloatField()
+#
+#	def get_price(self):
+#		pass
