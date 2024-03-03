fc299c23fe04e1afa0d80478a5272271256e64c8
afarut <renat030205@ya.ru>
2024-03-03 19:20:48 +0300

    add all models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-#from .models import Material, Range
+from .models import Material, Range, Detail, Order
 
 # Register your models here.
-#admin.site.register(Material)
-#admin.site.register(Range)
+admin.site.register(Material)
+admin.site.register(Range)
+admin.site.register(Detail)
+admin.site.register(Order)
