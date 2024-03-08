b2eee689ae6bb3d8b3c069d1d3566db67a785063
afarut <renat030205@ya.ru>
2024-03-08 12:28:33 +0300

    test commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
