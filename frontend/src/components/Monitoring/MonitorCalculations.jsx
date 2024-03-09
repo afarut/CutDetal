679b5d8104ce31ce22c9b9d7d4f4c4994f6a26c9
Daniil Markov <reedtzhere@gmail.com>
2024-03-10 02:37:27 +0300

    add pagination

--- frontend/src/components/Monitoring/MonitorCalculations.jsx
+++ frontend/src/components/Monitoring/MonitorCalculations.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import MonitoringListItem from "./MonitoringListItem";
 
-const MonitorCalculations = ({data}) => {
-  
-
+const MonitorCalculations = ({
+  data,
+  currentPage,
+  setCurrentPage,
+  next,
+  prev,
+}) => {
   return (
     <div>
       {data.map((el) => (
@@ -18,6 +22,21 @@ const MonitorCalculations = ({data}) => {
           key={el.id}
         />
       ))}
+
+      <div className="flex justify-center items-center mb-[10px]">
+        <div
+          onClick={() => setCurrentPage(currentPage - 1)}
+          className={`${!prev && 'hidden' } lg:px-[10px] lg:py-[8px] bg-blue-800 rounded-[8px] py-[4px] px-[8px] text-[14px] lg:text-[18px] text-white`}
+        >
+          Предыдущая страница
+        </div>
+        <div
+          onClick={() => setCurrentPage(currentPage + 1)}
+          className={`ml-[20px] ${!next && 'hidden' } lg:px-[10px] lg:py-[8px] bg-blue-800 rounded-[8px] py-[4px] px-[8px] text-[14px] lg:text-[18px] text-white`}
+        >
+          Следующая страница
+        </div>
+      </div>
     </div>
   );
 };
