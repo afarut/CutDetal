ef5bd679e41809ecd3ad928a43c6f52ece5a94ae
Daniil Markov <reedtzhere@gmail.com>
2024-03-13 00:37:37 +0300

    fixed monitoring orders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,7 +162,7 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.AdminRenderer",
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 5
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
