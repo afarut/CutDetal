31fc82d7d11d6b0e452ffd0b026c385768ce4c03
afarut <65365788+afarut@users.noreply.github.com>
2024-03-14 17:49:26 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,7 +162,8 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.AdminRenderer",
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DATETIME_FORMAT': "%Y-%m-%d %H:%M:%S",
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
