44b313a9d8e37a8046643856378a3f6806d7e05e
afarut <renat030205@ya.ru>
2024-03-14 14:08:34 +0300

    quick commit

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -1,11 +1,23 @@
-from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 
 class CreateOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method == "POST"
 
+
 class EditOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method == "PUT"
+
+
+class IsAuthAndSuperAdminOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.is_authenticated and request.user.is_superuser
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
