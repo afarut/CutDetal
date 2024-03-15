33a6fd72fe12330a5d0441717943bf9a1cb08e29
afarut <65365788+afarut@users.noreply.github.com>
2024-03-15 14:56:20 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -215,4 +215,4 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
