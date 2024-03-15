e3b4e794f51a5d08aa9a42922d8d31aad46df449
afarut <65365788+afarut@users.noreply.github.com>
2024-03-15 15:07:06 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -216,3 +216,4 @@ SIMPLE_JWT = {
 }
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
