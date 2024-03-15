b2adc367e4e0ca4f4c2a1d81d91db2ad0207d094
afarut <65365788+afarut@users.noreply.github.com>
2024-03-15 14:15:43 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -32,4 +32,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
